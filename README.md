006bcd45b0aa37a8230498737c7bb90908eba784
joshuaochia <joshuacosareochia@gmail.com>
2022-01-28 10:14:24 +0800

    docker initial set up

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Django web app on development
+
+Note: Create a .env for own development environment - .env.dist populated only required env for the web app.
+
+# Prerequisite
+
+- Docker 20.10.7
+
+### Set up the web app for development
+
+1. docker-compose build
+2. docker-compose up -d
+
+Helpful commands:
+
+1. docker-compose exec web python manage.py flush --no-input
+2. docker-compose exec web python manage.py migrate
--- app/app/settings.py
+++ app/app/settings.py
@@ -66,9 +66,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": env.str("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": env.str("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": env.str("SQL_USER", "user"),
+        "PASSWORD": env.str("SQL_PASSWORD", "password"),
+        "HOST": env.str("SQL_HOST", "localhost"),
+        "PORT": env.str("SQL_PORT", "5432"),
     }
 }
 
